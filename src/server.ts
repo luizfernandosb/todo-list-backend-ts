f1d97df7b2bf7d96dda06db9da3caf8bb75a566e
Luiz Fernando Sant'Ana <107377634+luizfernandosb@users.noreply.github.com>
2024-11-17 10:23:16 -0300

    fix: origin cors

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,12 @@ import todoRoutes from "./routes/todoRoutes";
 import exp from "constants";
 const cors = require("cors");
 
-
 dotenv.config();
 
 const app = express();
 app.use(
   cors({
-    origin: "https://todo-list-ts-theta-ashen.vercel.app/", // Permitir todas as origens (você pode restringir a origem se necessário)
+    origin: "https://todo-list-ts-theta-ashen.vercel.app", // Permitir todas as origens (você pode restringir a origem se necessário)
     methods: "GET, POST, PUT, PATCH, DELETE", // Métodos permitidos
     allowedHeaders: "Content-Type, Authorization, token", // Cabeçalhos permitidos
   })
@@ -22,5 +21,5 @@ app.use("/api/", todoRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta  ${PORT}`)
-})
+  console.log(`Servidor rodando na porta  ${PORT}`);
+});
